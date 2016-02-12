3c23f30574fd53290891eb114d843361440bc99c
juvatsana <julienvorabout@hotmail.fr>
2016-02-12 11:47:42 +0100

    Implements Page Add

--- HSecu/src/java/AddDMBean.java
+++ HSecu/src/java/AddDMBean.java
@@ -1,3 +1,8 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +15,11 @@
  */
 public class AddDMBean {
 
+    /* Data Base */
+    Connection connexion;
+    Statement statement;
+    ResultSet resultSet;
+    
     /* Infos */
     private String lastUpdate;
     private String lastRead;
